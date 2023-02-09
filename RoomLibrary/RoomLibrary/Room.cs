e91dec195a727e605ffd472ac0291bb0bf006bea
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-02-09 21:59:12 +0300

    Add files via upload

--- RoomLibrary/RoomLibrary/Room.cs
+++ RoomLibrary/RoomLibrary/Room.cs
@@ -46,5 +46,56 @@ namespace RoomLibrary
         {
             return RoomArea() / np;
         }
+        /// <summary>
+        /// Информация о комнате
+        /// </summary>
+        /// <returns>Возвращает строку</returns>
+        public string Info()
+        {
+            return "Комната площадью " + RoomArea() + "кв.м";
+        }
+    }
+    /// <summary>
+    /// Класс "жилая комната"
+    /// </summary>
+    public class LivingRoom : Room
+    {
+        int numWin; //число окон
+        public int Numwin
+        { get { return numWin; } set { numWin = value; } }
+        /// <summary>
+        /// Метод возвращает информацию о комнате
+        /// </summary>
+        /// <returns>Возвращается строка с информацией</returns>
+        public string Info()
+        {
+            return "Жилая комната площадью " + RoomArea() + "кв.м, с" + numWin + "окнами";
+        }
+    }
+    /// <summary>
+    /// Класс "офис"
+    /// </summary>
+    public class Oficce : Room
+    {
+        int numSockets; //число розеток
+        public int NumSockets
+        { get { return numSockets; } set { numSockets= value; } }
+        /// <summary>
+        /// Возвращает максимально возможное число рабочих мест
+        /// </summary>
+        /// <returns>Возвращает число мест</returns>
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+        /// <summary>
+        /// Метод возвращает информацию об офисе
+        /// </summary>
+        /// <returns></returns>
+        public string Info()
+        {
+            return "Офис на " + NumWorkplaces() + "рабочих мест";
+        }
     }
 }
